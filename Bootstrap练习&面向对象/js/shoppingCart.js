22205a1b86cf3c719a3e9c4b56640be77a0ef869
LDDZ <2603792595@qq.com>
2018-11-26 23:28:08 +0800

    完成设置购物订单项选择状态等功能
    完成功能设置购物订单项选择状态、删除某个指定（id）订单、某个指定订单数量加1/减1

--- Bootstrap练习&面向对象/js/shoppingCart.js
+++ Bootstrap练习&面向对象/js/shoppingCart.js
@@ -113,39 +113,60 @@ class ShoppingCart {
     return selectAmount;
   }
   // 设置购物订单项选择状态
-  setSelectStatus(id){
+  setItemSelectStatus(id, selectStatus) {
     let cartData = this.getDataFromLocalSatorge();
+    let flag = false;
     for (let i = 0; i < cartData.orderList.length; i++) {
       if (id == cartData.orderList[i].id) {
-        cartData.orderList[i].selectStatus=!cartData.orderList[i].selectStatus;
-        return cartData.orderList[i].selectStatus;
+        cartData.orderList[i].selectStatus = selectStatus;
+        flag = true;
       }
     }
-    return "无此ID";
+    if (flag) { this.setDataToLocalSatorge(cartData); }
+    else { console.log('状态修改失败，id有误！'); }
   }
 
   // 删除某个指定（id）订单
-  deleteAnOrder(id){
+  deleteItem(id) {
     let cartData = this.getDataFromLocalSatorge();
     for (let i = 0; i < cartData.orderList.length; i++) {
       if (id == cartData.orderList[i].id) {
-        localStorage.removeItem('cartData.orderList[i]');
-        return cartData.orderList[i];
+        console.log('要删除的订单下标为：' + i);
+        // 变更总商品总件数
+        cartData.totalQty -= cartData.orderList[i].qty;
+        //变更商品总价格
+        cartData.totalAmount -= cartData.orderList[i].qty * cartData.orderList[i].unitPrice;
+        // 变更总商品件数
+        cartData.units--;
+        // 删除当前订单
+        cartData.orderList.splice(i, 1);
+        break;
       }
     }
-    return "删除失败"
+    this.setDataToLocalSatorge(cartData);
   }
-  // 某个指定订单数量减1
-  AnOrderAdd(id){
+  // 某个指定订单数量加1/减1
+  AnOrderAdd(id, a) {
     let cartData = this.getDataFromLocalSatorge();
     for (let i = 0; i < cartData.orderList.length; i++) {
       if (id == cartData.orderList[i].id) {
-        cartData.orderList[i].qty--;
-        return cartData.orderList[i].qty;
+        if (a == '-') {
+          cartData.orderList[i].qty--;
+          cartData.totalQty--;
+          cartData.totalAmount -= cartData.orderList[i].unitPrice;
+          if (cartData.orderList[i].qty == 0) {
+            cartData.orderList.splice(i, 1);
+            cartData.units--;
+          }
+        } if (a == '+') {
+          cartData.orderList[i].qty++;
+          cartData.totalQty++;
+          cartData.totalAmount += cartData.orderList[i].unitPrice;
+        }
       }
     }
+    this.setDataToLocalSatorge(cartData);
   }
-  // 某个指定订单数量加1
 
   // 清空购物车（移除本地存储购物车项）
   clearShoppingCart() {
